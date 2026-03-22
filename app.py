894080cb5807459d91d760ab6d28ca8d159207e3
sooyee325-bit <sooyee325@gmail.com>
2026-03-22 16:07:01 +0800

    Add multiple function to multiply two numbers

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
-
+    
+def multiple(a, b):
+    return a * b
+    
 def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
