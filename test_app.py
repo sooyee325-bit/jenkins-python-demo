0e28bdf1a9095b24c478976c410ddadf10effc7f
sooyee325-bit <sooyee325@gmail.com>
2026-03-22 15:41:32 +0800

    Add test for multiple function

--- test_app.py
+++ test_app.py
@@ -8,6 +8,9 @@ def test_add():
 def test_divide():
     assert divide(10, 2) == 5
 
+def test_multiple():
+    assert multiple(10, 2) == 20
+
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         divide(10, 0)
